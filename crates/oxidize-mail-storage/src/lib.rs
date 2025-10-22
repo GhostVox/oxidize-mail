6fdcff275b0815035593a4e0282f7adbc534000a
Ghostvox <Ghostvox@icloud.com>
2025-10-22 13:48:43 -0400

    I have started implementing the oxidize-mail-parser, I created a types crate so we can share types across the project without creating a circular dependencies. Moved config.rs from oxidize-mail.iml to oxidize-mail-types

--- crates/oxidize-mail-storage/src/lib.rs
+++ crates/oxidize-mail-storage/src/lib.rs
@@ -1,3 +1,18 @@
+use oxidize_mail_types::AppConfig;
+use rusqlite::{params, Connection, Result};
+
+struct DB {
+    conn: Connection,
+}
+
+impl DB {
+    pub fn new(config: &Config) -> Result<Self, rusqlite::Error> {
+        Ok(Self {
+            conn: Connection::open(&config.app_db)?,
+        })
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
